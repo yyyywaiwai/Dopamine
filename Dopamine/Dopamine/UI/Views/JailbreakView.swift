a3101262521c4b15352ee1b194c0600a8a223136
Liam Huang <liamhuang0205@gmail.com>
2023-04-20 22:44:09 +0800

    [Dopamine] impl. respring & add ldrestart.

--- Dopamine/Dopamine/UI/Views/JailbreakView.swift
+++ Dopamine/Dopamine/UI/Views/JailbreakView.swift
@@ -65,6 +65,7 @@ struct JailbreakView: View {
         menuOptions = [
             .init(imageName: "gearshape", title: NSLocalizedString("Menu_Settings_Title", comment: ""), view: AnyView(SettingsView())),
             .init(imageName: "arrow.clockwise", title: NSLocalizedString("Menu_Restart_SpringBoard_Title", comment: ""), showUnjailbroken: false, action: respring),
+            .init(imageName: "arrow.counterclockwise.circle", title: "ldrestart", showUnjailbroken: false, action: doLdrestart),
             .init(imageName: "arrow.clockwise.circle", title: NSLocalizedString("Menu_Reboot_Userspace_Title", comment: ""), showUnjailbroken: false, action: userspaceReboot),
             .init(imageName: "arrow.clockwise.circle.fill", title: "Reboot", showUnjailbroken: false, action: doReboot),
             .init(imageName: "info.circle", title: NSLocalizedString("Menu_Credits_Title", comment: ""), view: AnyView(AboutView())),
