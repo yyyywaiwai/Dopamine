5de60649394c65453eb8095d6bae0e04ed0aae1f
Liam Huang <liamhuang0205@gmail.com>
2023-04-20 22:44:09 +0800

    [Dopamine] impl. respring & add ldrestart.

--- Dopamine/Dopamine/UI/Views/ContentView.swift
+++ Dopamine/Dopamine/UI/Views/ContentView.swift
@@ -62,6 +62,7 @@ struct ContentView: View {
         menuOptions = [
             .init(imageName: "gearshape", title: "Settings", view: AnyView(SettingsView())),
             .init(imageName: "arrow.clockwise", title: "Restart SpringBoard", showUnjailbroken: false, action: respring),
+            .init(imageName: "arrow.counterclockwise.circle", title: "ldrestart", showUnjailbroken: false, action: doLdrestart),
             .init(imageName: "arrow.clockwise.circle", title: "Reboot Userspace", showUnjailbroken: false, action: userspaceReboot),
             .init(imageName: "arrow.clockwise.circle.fill", title: "Reboot", showUnjailbroken: false, action: doReboot),
             .init(imageName: "info.circle", title: "Credits", view: AnyView(AboutView())),
--- Dopamine/Dopamine/UI/Views/Jailbreak.swift
+++ Dopamine/Dopamine/UI/Views/Jailbreak.swift
@@ -10,7 +10,45 @@ import Fugu15KernelExploit
 
 
 func respring() {
+    UIImpactFeedbackGenerator(style: .soft).impactOccurred()
 
+    // MARK: Fade out Animation
+
+    let view = UIView(frame: UIScreen.main.bounds)
+    view.backgroundColor = .black
+    view.alpha = 0
+
+    for window in UIApplication.shared.connectedScenes.map({ $0 as? UIWindowScene }).compactMap({ $0 }).flatMap({ $0.windows.map { $0 } }) {
+        window.addSubview(view)
+        UIView.animate(withDuration: 0.2, delay: 0, animations: {
+            view.alpha = 1
+        })
+    }
+
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: {
+        execCmd(args: ["/var/jb/usr/bin/killall", "-9", "backboardd"])
+    })
+}
+
+func doLdrestart() {
+    UIImpactFeedbackGenerator(style: .soft).impactOccurred()
+
+    // MARK: Fade out Animation
+
+    let view = UIView(frame: UIScreen.main.bounds)
+    view.backgroundColor = .black
+    view.alpha = 0
+
+    for window in UIApplication.shared.connectedScenes.map({ $0 as? UIWindowScene }).compactMap({ $0 }).flatMap({ $0.windows.map { $0 } }) {
+        window.addSubview(view)
+        UIView.animate(withDuration: 0.2, delay: 0, animations: {
+            view.alpha = 1
+        })
+    }
+
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: {
+        execCmd(args: ["/var/jb/usr/bin/ldrestart"])
+    })
 }
 
 func userspaceReboot() {
