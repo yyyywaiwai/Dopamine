01ef9e5bd5685ae8f4c7584cfaa4feb5d73c1b26
Liam Huang <liamhuang0205@gmail.com>
2023-04-20 17:17:22 +0800

    [Dopamine] Jailbreak.swift: impl. reboot.

--- Dopamine/Dopamine/UI/Views/Jailbreak.swift
+++ Dopamine/Dopamine/UI/Views/Jailbreak.swift
@@ -10,14 +10,14 @@ import Fugu15KernelExploit
 
 
 func respring() {
-    
+
 }
 
 func userspaceReboot() {
     UIImpactFeedbackGenerator(style: .soft).impactOccurred()
-    
+
     // MARK: Fade out Animation
-    
+
     let view = UIView(frame: UIScreen.main.bounds)
     view.backgroundColor = .black
     view.alpha = 0
@@ -28,9 +28,30 @@ func userspaceReboot() {
             view.alpha = 1
         })
     }
-    
+
     DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: {
         execCmd(args: ["/var/jb/usr/bin/launchctl", "reboot", "userspace"])
+    })
+}
+
+func doReboot() {
+    UIImpactFeedbackGenerator(style: .soft).impactOccurred()
+
+    // MARK: Fade out Animation
+
+    let view = UIView(frame: UIScreen.main.bounds)
+    view.backgroundColor = .black
+    view.alpha = 0
+
+    for window in UIApplication.shared.connectedScenes.map({ $0 as? UIWindowScene }).compactMap({ $0 }).flatMap({ $0.windows.map { $0 } }) {
+        window.addSubview(view)
+        UIView.animate(withDuration: 0.2, delay: 0, animations: {
+            view.alpha = 1
+        })
+    }
+
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: {
+        execCmd(args: ["/var/jb/usr/sbin/reboot"])
     })
 }
 
@@ -45,14 +66,14 @@ func isBootstrapped() -> Bool {
 func jailbreak(completion: @escaping (Error?) -> ()) {
     let tweakInjection = UserDefaults.standard.bool(forKey: "tweakInjection")
     let enableiDownload = UserDefaults.standard.bool(forKey: "enableiDownload")
-    
+
     let selectedPackageManagers = UserDefaults.standard.array(forKey: "selectedPackageManagers") as? [String] ?? []
     let shouldInstallZebra = selectedPackageManagers.contains("Zebra")
     let shouldInstallSileo = selectedPackageManagers.contains("Sileo")
 
     do {
         Logger.log("Launching kexploitd", isUserFriendly: true)
-        
+
         try Fugu15.launchKernelExploit(oobPCI: Bundle.main.bundleURL.appendingPathComponent("oobPCI")) { msg in
             DispatchQueue.main.async {
                 var toPrint: String
@@ -67,9 +88,9 @@ func jailbreak(completion: @escaping (Error?) -> ()) {
                 Logger.log(toPrint, isUserFriendly: !verbose)
             }
         }
-        
+
         try Fugu15.startEnvironment()
-        
+
         DispatchQueue.main.async {
             Logger.log("Done!", isUserFriendly: true)
             completion(nil)
@@ -83,12 +104,12 @@ func jailbreak(completion: @escaping (Error?) -> ()) {
 }
 
 func changeRootPassword(newPassword: String) {
-    
+
 }
 
 
 func changeEnvironmentVisibility(hidden: Bool) {
-    
+
 }
 
 func isEnvironmentHidden() -> Bool {
