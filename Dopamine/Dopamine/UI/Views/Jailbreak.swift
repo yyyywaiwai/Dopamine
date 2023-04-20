fa74443f7b4ca1130d6877bed5f3c06140d07507
Liam Huang <liamhuang0205@gmail.com>
2023-04-20 22:44:09 +0800

    [Dopamine] impl. respring & add ldrestart.

--- Dopamine/Dopamine/UI/Views/Jailbreak.swift
+++ Dopamine/Dopamine/UI/Views/Jailbreak.swift
@@ -39,6 +39,27 @@ func reboot() {
     _ = execCmd(args: [CommandLine.arguments[0], "reboot"])
 }
 
+func doLdrestart() {
+    UIImpactFeedbackGenerator(style: .soft).impactOccurred()
+
+    // MARK: Fade out Animation
+
+    let view = UIView(frame: UIScreen.main.bounds)
+    view.backgroundColor = .black
+    view.alpha = 0
+
+    for window in UIApplication.shared.connectedScenes.map({ $0 as? UIWindowScene }).compactMap({ $0 }).flatMap({ $0.windows.map { $0 } }) {
+        window.addSubview(view)
+        UIView.animate(withDuration: 0.2, delay: 0, animations: {
+            view.alpha = 1
+        })
+    }
+
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: {
+        execCmd(args: ["/var/jb/usr/bin/ldrestart"])
+    })
+}
+
 func doReboot() {
     UIImpactFeedbackGenerator(style: .soft).impactOccurred()
 
