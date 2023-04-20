c1dffc583151be48c61c17944b373930962f6f69
Liam Huang <liamhuang0205@gmail.com>
2023-04-20 17:18:03 +0800

    [Dopamine] ContentView.swift: add reboot button.

--- Dopamine/Dopamine/UI/Views/JailbreakView.swift
+++ Dopamine/Dopamine/UI/Views/JailbreakView.swift
@@ -64,6 +64,7 @@ struct JailbreakView: View {
             .init(imageName: "gearshape", title: NSLocalizedString("Menu_Settings_Title", comment: ""), view: AnyView(SettingsView())),
             .init(imageName: "arrow.clockwise", title: NSLocalizedString("Menu_Restart_SpringBoard_Title", comment: ""), showUnjailbroken: false, action: respring),
             .init(imageName: "arrow.clockwise.circle", title: NSLocalizedString("Menu_Reboot_Userspace_Title", comment: ""), showUnjailbroken: false, action: userspaceReboot),
+            .init(imageName: "arrow.clockwise.circle.fill", title: "Reboot", showUnjailbroken: false, action: doReboot),
             .init(imageName: "info.circle", title: NSLocalizedString("Menu_Credits_Title", comment: ""), view: AnyView(AboutView())),
         ]
     }
