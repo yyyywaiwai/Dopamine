4e0f9d152656709338b04a3ba020c22f7ea56416
Liam Huang <liamhuang0205@gmail.com>
2023-04-21 08:42:52 +0800

    [Update] better OTA update.

--- Dopamine/Dopamine/UI/Extensions/TextField++.swift
+++ Dopamine/Dopamine/UI/Extensions/TextField++.swift
@@ -124,3 +124,10 @@ extension View {
                          content: content)
     }
 }
+
+extension String {
+    func removePrefix(_ prefix: String) -> String {
+        guard self.hasPrefix(prefix) else { return self }
+        return String(self.dropFirst(prefix.count))
+    }
+}
