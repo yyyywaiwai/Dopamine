8b878e4d2de5df6ddbb9f7fe13ff0a94555e2f14
Liam Huang <liamhuang0205@gmail.com>
2023-05-02 00:13:27 +0800

    [MAPPING] path mapping.

--- BaseBin/jailbreakd/src/fakelib.h
+++ BaseBin/jailbreakd/src/fakelib.h
@@ -2,3 +2,5 @@ int setFakeLibVisible(bool visible);
 int makeFakeLib(void);
 bool isFakeLibBindMountActive(void);
 int setFakeLibBindMountActive(bool active);
+int64_t registerJbPrefixedPath(NSString *sourcePath, int retry);
+int64_t bindMountPath(NSString *sourcePath, bool check_existances);
