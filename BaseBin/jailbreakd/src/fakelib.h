135e46e69b4d32109ea349b0ccab1937b2ff60fd
Liam Huang <liamhuang0205@gmail.com>
2023-05-04 10:19:43 +0800

    [MAPPING] unmapping func.

--- BaseBin/jailbreakd/src/fakelib.h
+++ BaseBin/jailbreakd/src/fakelib.h
@@ -4,3 +4,4 @@ bool isFakeLibBindMountActive(void);
 int setFakeLibBindMountActive(bool active);
 int64_t registerJbPrefixedPath(NSString *sourcePath, int retry);
 int64_t bindMountPath(NSString *sourcePath, bool check_existances);
+int64_t bindUnmountPath(NSString *sourcePath);
